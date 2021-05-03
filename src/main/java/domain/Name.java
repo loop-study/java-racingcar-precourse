ba74c5c7347e3978bcb2dc8742903ca8c6e38316
apdkeb <apdkeb@naver.com>
2021-05-03 14:08:11 +0900

    feat(Name) : name 길이 확인
    2021. 05. 03.
    1. name 길이 1~5 확인하는 기능 추가
    
    Resolves : loop-study
    See also : NextStep

--- src/main/java/domain/Name.java
+++ src/main/java/domain/Name.java
@@ -1,13 +1,24 @@
 package domain;
 
 public class Name {
+    private static final String NAME_LENGTH_MESSAGE = "자동차 이름은 1 ~ 5글자 입니다.";
+    private static final int MAX_LENGTH = 5;
+
     private final String name;
 
     public Name(String name) {
+        validNameLength(name);
         this.name = name;
     }
 
     public String value() {
         return name;
+    }
+
+    private void validNameLength(String name) {
+        if (name.isEmpty()
+                || name.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException(NAME_LENGTH_MESSAGE);
+        }
     }
 }
