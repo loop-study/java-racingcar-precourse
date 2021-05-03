3249b5dd4a422ac626a72e285736e0a2675b05fe
apdkeb <apdkeb@naver.com>
2021-05-03 16:38:11 +0900

    test(Round) : 실패하는 Round 테스트 생성
    2021. 05. 03.
    - 게임 진행하는 Round 테스트 생성
      - 진행 확인 테스트
      - 종료 확인 테스트
    
    Resolves : loop-study
    See also : NextStep

---
+++ src/test/java/domain/RoundTest.java
@@ -0,0 +1,29 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("라운드")
+public class RoundTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    @DisplayName("진행 확인")
+    public void playTest(int count) throws Exception {
+        Round round = new Round(count);
+        round = round.next();
+        assertThat(round.isEnd()).isFalse();
+    }
+
+    @Test
+    @DisplayName("종료 확인")
+    public void endTest() throws Exception {
+        Round round = new Round(1);
+        round = round.next();
+        assertThat(round.isEnd()).isTrue();
+    }
+}
