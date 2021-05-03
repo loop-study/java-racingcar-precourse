10bc62f7d980b089b2222ed549078847c5ffa353
apdkeb <apdkeb@naver.com>
2021-05-03 17:35:17 +0900

    test(Record) : 우승자 테스트 추가
    2021. 05. 03.
    - 우승자 확인 테스트 추가
    
    Resolves : loop-study
    See also : NextStep

--- src/test/java/domain/RecordTest.java
+++ src/test/java/domain/RecordTest.java
@@ -17,6 +17,7 @@ public class RecordTest {
     public Record createRecord(int maxDistance) {
         List<Car> cars = new ArrayList<>();
         cars.add(new Car(NameTest.STUDY, new Distance(maxDistance)));
+        cars.add(new Car("povi"));
         return new Record(cars);
     }
 
@@ -27,5 +28,13 @@ public class RecordTest {
         Record record = createRecord(inputDistance);
         int maxDistance = record.maxDistance();
         assertThat(maxDistance).isEqualTo(inputDistance);
+    }
+
+    @Test
+    @DisplayName("우승자 확인")
+    public void winnersTest() throws Exception {
+        Record record = createRecord(1);
+        List<Car> winners = record.winners();
+        assertThat(winners.get(0).name().value()).isEqualTo(NameTest.STUDY.value());
     }
 }
