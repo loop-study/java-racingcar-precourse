e0e0a2a139d41b823d2db071f0f80b0792f0c64d
apdkeb <apdkeb@naver.com>
2021-05-03 14:27:47 +0900

    refect : DistanceTest 이동거리 테스트 수정
    2021. 05. 03.
    - 이동거리 테스트 배열 방식으로 변경
    
    Resolves : loop-study
    See also : NextStep

--- src/test/java/domain/DistanceTest.java
+++ src/test/java/domain/DistanceTest.java
@@ -10,11 +10,16 @@ import static org.assertj.core.api.Assertions.*;
 @DisplayName("이동거리")
 public class DistanceTest {
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(ints = {0,1,2,3})
     @DisplayName("이동거리 확인")
-    public void distanceTest() throws Exception {
-        Distance distance = new Distance().add();
+    public void distanceTest(int moveCount) throws Exception {
+        Distance distance = new Distance();
 
-        assertThat(distance.value()).isEqualTo(1);
+        for (int i = 0; i < moveCount; i++) {
+            distance = distance.add();
+        }
+
+        assertThat(distance.value()).isEqualTo(moveCount);
     }
 }
