e5602cc1c0cb88a474a7866a022c95154e81c761
apdkeb <apdkeb@naver.com>
2021-10-07 20:40:33 +0900

    feat : Cars 유효성 추가
    2021. 10. 07.
    - 2대 미만 여부 로직 추가
    - 관련 테스트 추가
    
    Resolver : loop-study
    See also : NEXTSTEP

--- src/main/java/racinggame/domain/Cars.java
+++ src/main/java/racinggame/domain/Cars.java
@@ -6,9 +6,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cars {
+    private static final Integer CARS_MIN_SIZE = 2;
+    private static final String ERROR_CARS_SIZE_MESSAGE = "자동차는 2대 이상 있어야합니다.";
+
     private final List<Car> cars;
 
     public Cars(List<String> carNames, Strategy strategy) {
+        validationCarsSize(carNames);
         this.cars = createCars(carNames, strategy);
     }
 
@@ -18,5 +22,11 @@ public class Cars {
             cars.add(new Car(carName, strategy));
         }
         return cars;
+    }
+
+    private void validationCarsSize(List<String> carNames) {
+        if (CARS_MIN_SIZE > carNames.size()) {
+            throw new IllegalArgumentException(ERROR_CARS_SIZE_MESSAGE);
+        }
     }
 }
--- src/test/java/racinggame/domain/CarsTest.java
+++ src/test/java/racinggame/domain/CarsTest.java
@@ -1,12 +1,16 @@
 package racinggame.domain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class CarsTest {
+
     @Test
     void 자동차_여러대_생성() {
         String inputCarNames = "java,pobi,jason,loop";
@@ -14,5 +18,10 @@ public class CarsTest {
         assertThat(cars).isNotNull();
     }
 
-
+    @ParameterizedTest
+    @ValueSource(strings = {",", "a"})
+    void 자동차_2대미만_예외확인(String names) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Cars(Arrays.asList(names.split(",")), new MoveStrategy()));
+    }
 }
