738ce157723fcf6f52cfa74433e11f506e54e3f6
apdkeb <apdkeb@naver.com>
2021-10-07 17:16:40 +0900

    feat : 자동차 생성
    2021. 10. 07.
    - Car 로직 추가.
    - README.md 요구사항 추가
    
    Resolver : loop-study
    See also : NEXTSTEP

---
+++ src/main/java/racinggame/domain/Car.java
@@ -0,0 +1,12 @@
+package racinggame.domain;
+
+public class Car {
+
+    private String carName;
+    private int distance;
+
+    public Car(String carName) {
+        this.carName = carName;
+        this.distance = 0;
+    }
+}
